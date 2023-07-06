e5e2e45288d5ee539bf74d2e16c3dd6c6c48de7d
sai krishna <saikrishnacheruku2848@gmail.com>
2023-07-06 22:27:57 +0530

    javascript project

---
+++ Task-Mgmt-project/index.js
@@ -0,0 +1,12 @@
+const obj1={
+    name:"sai",
+    age:200,
+    principle:"coding"
+}
+const obj2={
+    name:"sravani",
+    age:"28",
+    like :"being happy"
+}
+
+const secobj=Object.assign(obj1,obj2);
